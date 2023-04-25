268e4e79476e1f9b266c9583649431dc5477fada
Garsal9731 <9731@cifpceuta.es>
2023-04-25 12:40:49 +0200

    Cambios Iniciales Para Movil

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estiloMovil.css">
+    <title>Version Movil</title>
+    </head>
+    <body>
+        <div class="parteIzquierda">
+            <script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.304/build/spline-viewer.js"></script>
+            <spline-viewer url="https://prod.spline.design/qnsa6Qt0IrvbKd-1/scene.splinecode"></spline-viewer>
+        </div>
+        <div class="parteDerecha">
+        <h1>BIENVENIDOS A MI PAGINA</h1>
+        <div class="formulario">
+            <input type="text" name="" id="" placeholder="Correo Electrónico">
+            <input type="text" name="" id="" placeholder="Contraseña">
+            <input type="text" class="button" value="Iniciar Sesión">
+            <hr>
+            <input type="button" name="" id="" value="Crear Cuenta">
+            <input type="button" name="" id="" value="Ver Aspirantes">
+            <!-- if(issset$registra){
+                echo doForm
+            }else{
+                echo
+            }-->
+        </div>
+        </div>
+    </body>
+</html>
