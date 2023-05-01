4e8ade8527c8a2fe2933136439f5bc551f511184
Sxcram02 <ms2d0v4@gmail.com>
2023-05-01 03:00:31 +0200

    Refaztorización y optimización del Js, HTML y CSS

--- public/js/funciones.inc.js
+++ public/js/funciones.inc.js
@@ -3,59 +3,16 @@ function mostrarContrasenya() {
     (x.type === "password")?x.type = "text":x.type = "password";
 }
 
-const botonAgregarFormacion = document.getElementById("agregarFormacion");
-const contenedorFormacion = document.getElementById("contenedorFormacion");
-
-botonAgregarFormacion.addEventListener("click", () => {
+function createOption($id,$class){
+    const contenedorFormacion = document.getElementById($id);
     const nuevoContenedor = contenedorFormacion.firstElementChild.cloneNode(true);
-    nuevoContenedor.querySelector("input[name='anioInicioFormacion']").value = "";
-    nuevoContenedor.querySelector("input[name='anioFinalFormacion']").value = "";
-    nuevoContenedor.querySelector("input[name='cursoAutoformacion']").value = "";
+    nuevoContenedor.querySelector($class).value = "";
     contenedorFormacion.appendChild(nuevoContenedor);
-});
+};
 
-const botonQuitarFormacion = document.getElementById("quitarFormacion");
-
-botonQuitarFormacion.addEventListener("click", () => {
-    const contenedoresFormacion = document.querySelectorAll("#contenedorFormacion .contenedorInputs");
+function deleteOption($id){
+    const contenedoresFormacion = document.querySelectorAll($id + " .contenedorInputs");
     if(contenedoresFormacion.length > 1){
         contenedoresFormacion[contenedoresFormacion.length-1].remove();
     }
-});
-const botonAgregarExperiencia = document.getElementById("agregarExperiencia");
-const contenedorExperiencia = document.getElementById("contenedorExperiencia");
-
-botonAgregarExperiencia.addEventListener("click", () => {
-    const nuevoContenedor = contenedorExperiencia.firstElementChild.cloneNode(true);
-    nuevoContenedor.querySelector("input[name='fechaInicioExperiencia']").value = "";
-    nuevoContenedor.querySelector("input[name='fechaFinalExperiencia']").value = "";
-    nuevoContenedor.querySelector("input[name='descripcionExperiencia']").value = "";
-    contenedorExperiencia.appendChild(nuevoContenedor);
-});
-const botonQuitarExperiencia = document.getElementById("quitarExperiencia");
-
-botonQuitarExperiencia.addEventListener("click", () => {
-const contenedoresExperiencia = document.querySelectorAll("#contenedorExperiencia .contenedorInputs");
-if(contenedoresExperiencia.length > 1){
-    contenedoresExperiencia[contenedoresExperiencia.length-1].remove();
-}
-});
-
-const botonAgregarIdioma = document.getElementById("agregarIdioma");
-const contenedorIdiomas = document.getElementById("contenedorIdiomas");
-
-botonAgregarIdioma.addEventListener("click", () => {
-    const nuevoContenedor = contenedorIdiomas.firstElementChild.cloneNode(true);
-    nuevoContenedor.querySelector("input[name='idioma']").value = "";
-    nuevoContenedor.querySelector("select[name='nivelIdioma']").selectedIndex = 0;
-    contenedorIdiomas.appendChild(nuevoContenedor);
-});
-
-const botonQuitarIdioma = document.getElementById("quitarIdioma");
-
-botonQuitarIdioma.addEventListener("click", () => {
-    const contenedoresIdiomas = document.querySelectorAll("#contenedorIdiomas .contenedorInputs");
-    if(contenedoresIdiomas.length > 1){
-        contenedoresIdiomas[contenedoresIdiomas.length-1].remove();
-    }
-});
+};
