0b6e5a39c282072d23e54e35a3370ce0ff1d88c0
Sxcram02 <ms2d0v4@gmail.com>
2023-05-16 14:15:52 +0200

    modelo usuario

--- src/model/usuarios.php
+++ src/model/usuarios.php
@@ -1,6 +1,8 @@
 <?php
+
+
 class Usuario {
-    private string $email,$contrasenia, $apellido2;
+    protected string $email,$contrasenia, $apellido2;
     public string $nombreUser,$apellido1;
 
     public function __construct($email,$nombreUser,$apellido1,$contrasenia){
@@ -27,18 +29,62 @@ class Usuario {
     }
 }
 
+
+
 class Aspirantes extends Usuario {
     private string $emailAspirante, $dni;
     private int $codExpediente;
 
     public string $fechaNac, $familia, $grado;
 
-    public function __construct($emailAspirante,$dni,$fechaNac,$familia,$grado){
+    public function __construct($dni,$fechaNac,$familia,$grado){
+        $emailAspirante = $this -> email;
         $this -> emailAspirante = $emailAspirante;
         $this -> dni = $dni;
         $this -> fechaNac = $fechaNac;
         $this -> familia = $familia;
         $this -> grado = $grado;
+    }
+
+    public function getEmailAspirante(){
+        return $this -> emailAspirante;
+    }
+
+    public function getDniAspirante(){
+        return $this -> dni;
+    }
+
+    public function getFechaNacAspirante(){
+        return $this -> fechaNac;
+    }
+
+    public function getFamiliaAspirante(){
+        return $this -> familia;
+    }
+
+    public function getGradoAspirante(){
+        return $this -> grado;
+    }
+}
+
+
+
+
+class Empresario extends Usuario {
+    private int $codEmpresa;
+    private string $emailEmpresario;
+    public function __construct($codEmpresa){
+        $emailEmpresario = $this -> email;
+        $this -> codEmpresa = $codEmpresa;
+        $this -> emailEmpresario = $emailEmpresario;
+    }
+
+    public function getCodEmpresa(){
+        return $this -> codEmpresa;
+    }
+
+    public function getEmailEmpresario(){
+        return $this -> emailEmpresario;
     }
 }
 
